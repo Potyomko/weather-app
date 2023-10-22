02b201f20355a1792831e6dbad9f224c74a70d4b
Olexander <cmurtimur@gmail.com>
2023-10-22 17:11:31 +0300

    Api new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import { GlobalStyle } from "../GlobalStyle";
+import { HeroAPI } from "./Hero/HeroAPI";
 export const App = () => {
   return (
     <div>
+      <HeroAPI/>
       <GlobalStyle/>
     </div>
   );
--- src/components/Hero/HeroAPI.jsx
+++ src/components/Hero/HeroAPI.jsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 
 export const HeroAPI = ()=>{
 
     const [weatherData, setWeatherData] = useState(null); 
 
 useEffect(()=>{
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=018399e824faf2110a59974f80cc6c36')
-    .then((response) => response.json())
-    .then((data) => {
-        setWeatherData(data);
-      })
-})
+    const a = false
+   
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=018399e824faf2110a59974f80cc6c36')
+        .then((response) => response.json())
+        .then((data) => {
+            setWeatherData(data);
+          })
+   
+}, [] )
 
 }
