9b6faff3064baaa7d0ad81c185779fc8c4223a9a
Oleh Tsap <olehtsap07@gmail.com>
2023-10-24 22:31:37 +0300

    start write the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { GlobalStyle } from "../GlobalStyle";
 import { Hero } from "./Hero/Hero";
 import { HeroAPI } from "./Hero/HeroAPI";
+import { News } from "./News/News";
 export const App = () => {
   return (
     <div>
---
+++ src/components/News/News.jsx
@@ -0,0 +1,29 @@
+export const News = () => {
+
+    
+
+    return (
+        <>
+            <div>
+                <ul>
+                    <li>
+                        <img src="" alt="" />
+                        <p></p>
+                    </li>
+                    <li>
+                        <img src="" alt="" />
+                        <p></p>
+                    </li>
+                    <li>
+                        <img src="" alt="" />
+                        <p></p>
+                    </li>
+                    <li>
+                        <img src="" alt="" />
+                        <p></p>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
