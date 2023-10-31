e3a2110194d54795fd6ec4f593d923aca7020fb2
Sophia <melnichenko.cofia07@gmail.com>
2023-10-31 21:41:41 +0200

    get some info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { GlobalStyle } from "../GlobalStyle";
 import { Hero } from "./Hero/Hero";
 import { HeroAPI } from "./Hero/HeroAPI";
+import {MoreInfoList } from './MoreInfo/MoreInfoList.jsx';
 export const App = () => {
   return (
     <div>
