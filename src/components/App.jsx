71989ba0ef6c608573a3fed343884be3a35c8f16
Oleh Tsap <olehtsap07@gmail.com>
2023-10-31 19:23:44 +0200

    add News in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { GlobalStyle } from "../GlobalStyle";
+import { News } from "./News/News";
 import { Hero } from "./Hero/Hero";
 import { HeroAPI } from "./Hero/HeroAPI";
 export const App = () => {
   return (
     <div>
-      <Hero/>
-    
+      <Hero />
+      <News/>
+
       <GlobalStyle/>
     </div>
   );
