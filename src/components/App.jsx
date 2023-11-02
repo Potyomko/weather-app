2cac662e18e8b25e3db1bb029036df5021a5b54f
Potyomko Veronika <vnpotomko@gmail.com>
2023-11-02 18:56:51 +0200

    finish styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { GlobalStyle } from "../GlobalStyle";
 import { News } from "./News/News";
 import { Hero } from "./Hero/Hero";
-import { HeroAPI } from "./Hero/HeroAPI";
-import {MoreInfoList } from './MoreInfo/MoreInfoList.jsx';
+// import { HeroAPI } from "./Hero/HeroAPI";
+// import {MoreInfoList } from './MoreInfo/MoreInfoList.jsx';
 import {Footer} from './Footer/Footer';
 import { WeatherForecastForWeek } from "./WeatherForecastForWeek/WeatherForecastForWeek";
 import { Header } from "./Header/Header";
@@ -12,7 +12,7 @@ export const App = () => {
     <div>
       <Header />
       <Hero/>
-      <WeatherForecastForWeek />
+      {/* <WeatherForecastForWeek /> */}
       <News/>
       <Footer/>
       <GlobalStyle/>
