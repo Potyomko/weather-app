dfd62739d330bf6b9929b180c4a6c6167aa0979c
Vlad <vladikhoncharuk@gmail.com>
2023-11-07 16:31:24 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import { GlobalStyle } from "../GlobalStyle";
 import { Hero } from "./Hero/Hero";
 import { HeroAPI } from "./Hero/HeroAPI";
 import {Footer} from './Footer/Footer';
+import Gallery from "./Gallery/Gallery";
 
 export const App = () => {
   return (
     <div>
       <Hero/>
+      <Gallery />
       <Footer/>
       <GlobalStyle/>
     </div>
---
+++ src/components/Gallery/Gallery.styled.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import { Theme } from '../Theme';
+
+export const Title = styled.h2`
+    color: ${Theme.colors.black};
+    font-family: ${Theme.fonts.heading};
+    font-size: 20px;
+    font-weight: ${Theme.fontWeights.medium};
+    margin-bottom: 40px;
+`;
+
+export const Container = styled.div`
+    max-width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 80px 0;
+
+    @media (max-width: 1200px){
+        display: none;
+    }
+`;
+
+export const Image = styled.img`
+    width: 384px;
+    height: 211px;
+    object-fit: cover;
+`
