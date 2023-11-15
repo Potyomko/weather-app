1cd8ef073aec1162c47c23a6f3b935a5ea3573a2
Sophia <melnichenko.cofia07@gmail.com>
2023-11-15 17:02:31 +0200

    component for all info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,21 @@ import { News } from "./News/News";
 import { Hero } from "./Hero/Hero";
 // import { HeroAPI } from "./Hero/HeroAPI";
 // import {MoreInfoList } from './MoreInfo/MoreInfoList.jsx';
-import {Footer} from './Footer/Footer';
+import { Footer } from './Footer/Footer';
 import Gallery from "./Gallery/Gallery";
 import { WeatherForecastForWeek } from "./WeatherForecastForWeek/WeatherForecastForWeek";
 import { Header } from "./Header/Header";
-import { MoreInfoList } from "./MoreInfo/MoreInfoList";
-import { MoreInfoChartComponent } from "./MoreInfo/MoreInfoChartComponent";
+import { SomeInfo } from "./SomeInfo/SomeInfo";
 
 export const App = () => {
   return (
     <div>
       <Header />
       <Hero />
-      <News/>
-      <Footer/>
-      <GlobalStyle/>
+      <SomeInfo />
+      <News />
+      <Footer />
+      <GlobalStyle />
     </div>
   );
 };
