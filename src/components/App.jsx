bba89ddcc40175576d58273a693034b8c530657a
Niky-Niky <nik333nik888@gmail.com>
2023-11-21 20:19:42 +0100

    my commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { GlobalStyle } from "../GlobalStyle";
 import { News } from "./News/News";
 import { Hero } from "./Hero/Hero";
 // import { HeroAPI } from "./Hero/HeroAPI";
 // import {MoreInfoList } from './MoreInfo/MoreInfoList.jsx';
 import { SomeInfo } from "./SomeInfo/SomeInfo";
+import {Weather} from '../components/'
 import {Footer} from './Footer/Footer';
 import Gallery from "./Gallery/Gallery";
 import { WeatherForecastForWeek } from "./WeatherForecastForWeek/WeatherForecastForWeek";
@@ -13,16 +15,17 @@ import { MoreInfoList } from "./MoreInfo/MoreInfoList";
 import { MoreInfoChartComponent } from "./MoreInfo/MoreInfoChartComponent";
 
 export const App = () => {
+  const [cityName, setCityName] = useState('')
+  const getCityName = (city) => {
+    setCityName(city)
+  }
   return (
     <div>
       <Header />
-<<<<<<< Updated upstream
       <Hero />
       <SomeInfo />
-=======
       <Hero getCityName={getCityName}/>
       <Weather cityName={cityName}/>
->>>>>>> Stashed changes
       {/* {console.log(<WeatherForecastForWeek/>)} */}
       {/* <WeekForecastLocationAPI theCity={'Kyiv'}/> */}
       <SomeInfo />
