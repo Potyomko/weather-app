8a9c8e38dbdc097f7324ef0211822f3f1bad4bfa
Potyomko Veronika <vnpotomko@gmail.com>
2023-11-26 11:07:06 +0200

    styles

--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -20,7 +20,9 @@ export const FooterAdressDiv = styled.div`
         padding: 30px 0px 0px 0px;
     }
     @media screen and (min-width: 1200px){
-        padding: 41px 0px 0px 0px;
+        display: flex;
+        padding: 42px 0px 0px 0px;
+        flex-direction: column;
     }
     
 `
@@ -69,13 +71,13 @@ export const FooterContactUsDiv = styled.div`
         display: flex;
         flex-direction: column;
         align-items: flex-start;
-        padding: 30px 0px 0px 79px;
+        padding: 30px 0px 0px 153px;
     }
     @media screen and (min-width: 1200px){
         display: flex;
-        flex-direction: column;
+        flex-direction: column; 
         align-items: flex-start;
-        padding: 41px 0px 0px 132px;
+        padding: 44px 0px 0px 192px;
     }
 `
 export const FooterContactUsTitle = styled.p`
@@ -101,33 +103,49 @@ export const FooterIconsDiv = styled.div`
         width: 145px;
         padding: 20px;
     }
-    @media screen and(min-width: 1200px){
-        width: 161px;
-    }
 `
 export const FooterIconInstagram = styled.img`
-    width: 60px;
-    height: 60px;
-    padding-top: 10px;
-    padding-right: 10px;
-    padding-bottom: 10px;
+    @media screen and (min-width: 1200px){
+        width: 40px;
+        height: 40px;
+        padding-top: 10px;
+        padding-right: 10px;
+    }
+    @media screen and (min-width: 768px){
+        width: 35px;
+        height: 35px;
+        padding-top: 5px;
+        padding-right: 5px;
+    }
 
 `
 export const FooterIconFacebook = styled.img`
-    width: 60px;
-    height: 60px;
-    padding-top: 10px;
-    padding-right: 10px;
-    padding-left: 10px;
-    padding-bottom: 10px;
+    @media screen and (min-width: 1200px){
+        width: 40px;
+        height: 40px;
+        padding-top: 10px;
+        padding-right: 10px;
+    }
+    @media screen and (min-width: 768px){
+        width: 35px;
+        height: 35px;
+        padding-top: 5px;
+        padding-right: 5px;
+    }
 `
 export const FooterIconWhatsapp = styled.img`
-    width: 60px;
-    height: 60px;
-    padding-top: 10px;
-    padding-right: 10px;
-    padding-left: 10px;
-    padding-bottom: 10px;
+    @media screen and (min-width: 1200px){
+        width: 40px;
+        height: 40px;
+        padding-top: 10px;
+        padding-right: 10px;
+    }
+    @media screen and (min-width: 768px){
+        width: 35px;
+        height: 35px;
+        padding-top: 5px;
+        padding-right: 5px;
+    }
 `
 export const FooterIconLogoDiv = styled.div`
     padding: 40px 150px 77px 103px;
@@ -136,9 +154,10 @@ export const FooterIconLogoDiv = styled.div`
         padding: 20px 0px 0px 21px;
     }
     @media screen and (min-width: 768px){
-        padding: 35px 100px 63px 100px;
+        padding: 35px 0px 49px 86px;
     }
     @media screen and (min-width: 1200px){
-        padding: 40px 150px 77px 103px;
+        display: flex;
+        padding: 46px 0px 74px 128px;
     }
 `
