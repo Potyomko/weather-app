5a115793b7e944150972e56eed1ac88dd868fea3
Niky-Niky <nik333nik888@gmail.com>
2023-11-27 22:26:26 +0100

    making date relevant

--- src/components/WeatherForecastForWeek/WeatherForecastLocationAPI.jsx
+++ src/components/WeatherForecastForWeek/WeatherForecastLocationAPI.jsx
@@ -2,17 +2,18 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { WeatherForecastForWeek } from "./WeatherForecastForWeek";
 
+export const formatDate = timestamp => {
+    const date = new Date(timestamp * 1000);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+};
+
 export const WeekForecastLocationAPI = ({theCity}) => {
     const [lat, setLat] = useState('');
     const [lon, setLon] = useState('');
     const [weather, setWeather] = useState('');
-    const [date, setDate] = useState('');
+    // const [date, setDate] = useState('');
 
-    // const formatDate = timestamp => {
-    //     const date = new Date(timestamp * 1000);
-    //     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    //     return date.toLocaleDateString('en-US', options);
-    //   };
 
 useEffect(() => {
     console.log('theCity: ', theCity)
@@ -25,9 +26,9 @@ useEffect(() => {
 
         const myCityDate = JSON.parse(window.localStorage.getItem('cities')).find(oneCity => oneCity.dt);
         
-        if(myCityDate){
-            setDate(new Date(myCityDate * 1000))
-        }
+        // if(myCityDate){
+        //     setDate(new Date(myCityDate * 1000))
+        // }
 
         if(lat && lon && theCity){
             fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=40207e285e43c5b8e49ba7f2599cdd4b`)
@@ -47,7 +48,7 @@ return(
     <>
     {console.log(weather)}
     {weather && <WeatherForecastForWeek theWeather={weather}/>}
-    {console.log(date)}
+    {/* {console.log(date)} */}
     </>
 )
 }
