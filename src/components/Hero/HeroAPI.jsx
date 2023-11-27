b4f224b592d9b41c34532b65080121fb94f8b1d2
Olexander <cmurtimur@gmail.com>
2023-11-27 20:04:32 +0200

    пофіксив то що казала іра

--- src/components/Hero/HeroAPI.jsx
+++ src/components/Hero/HeroAPI.jsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
 
-export const HeroAPI = ({ city }) => {
-  const [weather, setWeatherData] = useState([]);
+export const heroAPI = (cityName) => {
+  let WeatherData = JSON.parse(localStorage.getItem('cities')) || [];; 
+  
+  console.log(cityName);
 
-  useEffect(() => {
-    if (city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=018399e824faf2110a59974f80cc6c36`)
-        .then((response) => response.json())
-        .then((data) => {
-          setWeatherData((prevWeather) => [...prevWeather, data]);
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=018399e824faf2110a59974f80cc6c36`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (WeatherData) {
+        WeatherData = [...WeatherData, data];
+      } 
 
-          const citiesWeather = JSON.parse(localStorage.getItem('cities')) || [];
-          citiesWeather.push(data);
-          localStorage.setItem('cities', JSON.stringify(citiesWeather));
-        })
-        .catch((error) => {
-          console.error('Помилка при отриманні даних погоди:', error);
-        });
-    }
-  }, [city]);
-}
+      // const citiesWeather = JSON.parse(localStorage.getItem('cities')) || [];
+   
+      localStorage.setItem('cities', JSON.stringify(WeatherData)); 
+    })
+    .catch((error) => {
+      console.error('Помилка при отриманні даних погоди:', error);
+    });
+};
